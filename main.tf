ea6bb08568d0c7c6ffc9ff99245bb4df5e1df77d
Bkoji1150 <kojibello058@gmail.com>
2022-09-02 11:06:09 -0700

    added

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ locals {
   state_bucket_key = format("env:/%s/path/env", terraform.workspace)
 }
 
+
 module "helm_configuration" {
   source = "./modules"
 
@@ -27,4 +28,20 @@ module "helm_configuration" {
   scaling_runners       = csvdecode(file("template/scaling_runners.csv"))
   organizations_runners = csvdecode(file("template/organizations_runners.csv"))
  repositories_runners =  csvdecode(file("template/repositories_runners.csv"))
+}
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["APP_VPC"]
+  }
+}
+
+# Pulling down priv_subnet 
+data "aws_subnet_ids" "priv_subnet" {
+  vpc_id = data.aws_vpc.vpc.id
+  filter {
+    name   = "tag:Name"
+    values = ["APP_VPC-private-*"]
+  }
 }
